411919f936aaaef578c654ba475b76245242b148
Lauryn Kim <laurynapr99@gmail.com>
2021-03-20 01:37:03 +0900

    feature(user PUT route & store): adds API route and thunk creator for updating user info, updates location-verification component

--- client/store/user.js
+++ client/store/user.js
@@ -6,6 +6,7 @@ import history from '../history'
  */
 const GET_USER = 'GET_USER'
 const REMOVE_USER = 'REMOVE_USER'
+const UPDATE_USER = 'UPDATE_USER'
 
 /**
  * INITIAL STATE
@@ -17,6 +18,7 @@ const defaultUser = {}
  */
 const getUser = user => ({type: GET_USER, user})
 const removeUser = () => ({type: REMOVE_USER})
+const _updateUser = user => ({type: UPDATE_USER, user})
 
 /**
  * THUNK CREATORS
@@ -56,6 +58,15 @@ export const logout = () => async dispatch => {
   }
 }
 
+//GENERIC THUNK CREATOR FOR ALL USER UPDATES (updating info in settings, adding location coords after verification)
+//user argument passed with spread operator of existing user info + updated user info
+export const updateUser = user => {
+  return async dispatch => {
+    const {data} = await axios.put('/api/users', user)
+    dispatch(_updateUser(data))
+  }
+}
+
 /**
  * REDUCER
  */
@@ -65,6 +76,8 @@ export default function(state = defaultUser, action) {
       return action.user
     case REMOVE_USER:
       return defaultUser
+    case UPDATE_USER:
+      return action.user
     default:
       return state
   }
--- server/api/users.js
+++ server/api/users.js
@@ -2,15 +2,64 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+// GET /api/users
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
+      attributes: [
+        'id',
+        'firstName',
+        'lastName',
+        'email',
+        'defaultImage',
+        'reviewScore',
+        'photoVerified',
+        'emailVerified',
+        'locationVerified'
+      ]
     })
-    res.json(users)
+    res.status(200).send(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// GET single User
+// GET /api/users/:userId
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      attributes: [
+        'id',
+        'firstName',
+        'lastName',
+        'email',
+        'defaultImage',
+        'reviewScore',
+        'photoVerified',
+        'emailVerified',
+        'locationVerified'
+      ],
+      where: {id: req.params.userId}
+    })
+    if (!user) {
+      res.status(404).send('This user does not exist!')
+    }
+    res.status(200).send(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// PUT api/users
+router.put('/', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.user.id)
+    const updatedUser = await user.update(req.body)
+    res.status(201).send(updatedUser)
   } catch (err) {
     next(err)
   }
