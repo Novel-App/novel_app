cc97273ff02e39e37d6f3fd6964ee75732bcad2e
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-03-22 23:13:30 -0400

    feat: added single product view

--- client/components/Products/SingleProducts.js
+++ client/components/Products/SingleProducts.js
@@ -3,3 +3,62 @@
 //image slides
 //body card: title, author, isbn, genre, condition, condition info icon to hover, description
 //bottom card: favorites button, numFavorites, price, can bargain, chat button
+
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchSingleProduct} from '../../store/product'
+
+class SingleProduct extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true
+    }
+  }
+  componentDidMount() {
+    //or params or the product passed in from props
+    let productId = this.props.match.params.id
+    this.props.loadProduct(productId)
+    this.setState({loading: false})
+  }
+  render() {
+    if (this.state.loading === true) {
+      return (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      )
+    }
+    const product = this.props.product
+    const barginStatus = product.canBargin
+      ? 'Price negotiable'
+      : 'Price non-negotiable'
+    return (
+      <>
+        <img className="card-img-top" alt={product.title} src={product.image} />
+        <h1 className="card-title">{product.title}</h1>
+        <h2 className="card-subtitle mb-2 text-muted">by {product.author}</h2>
+        <p>{product.createdAt}</p>
+        <p>{barginStatus}</p>
+        <p>${product.price}</p>
+        <p>{product.description}</p>
+        <p>♡: {product.numFavorites}</p>
+      </>
+    )
+  }
+}
+
+const mapState = state => {
+  return {
+    user: state.user,
+    product: state.products.single
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    loadProduct: productId => dispatch(fetchSingleProduct(productId))
+  }
+}
+
+export default connect(mapState, mapDispatch)(SingleProduct)
