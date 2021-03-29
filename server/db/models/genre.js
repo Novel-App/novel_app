09ba778c7a14c77bc14628f3b15d5b19f7ff62b0
akmr328 <akm328@gmail.com>
2021-03-29 16:04:19 -0400

    chat messages sending and receiving

--- server/db/models/genre.js
+++ server/db/models/genre.js
@@ -2,12 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Genre = db.define('genre', {
-  id: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: Sequelize.INTEGER
-  },
+  // id: {
+  //   allowNull: false,
+  //   autoIncrement: true,
+  //   primaryKey: true,
+  //   type: Sequelize.INTEGER
+  // },
   isFiction: {
     type: Sequelize.BOOLEAN,
     allowNull: false
