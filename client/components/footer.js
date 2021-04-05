a2bf947b1802b5f36d21753024a46a188cdad1d0
akmr328 <akm328@gmail.com>
2021-04-04 23:59:34 -0400

    adds footer

---
+++ client/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => (
+  <div className="footer">
+    <i className="bi bi-instagram" />
+    <span />
+    <i className="bi bi-github" />
+    <span />
+    <i className="bi bi-envelope-fill">Contact Us</i>
+  </div>
+)
+
+export default Footer
