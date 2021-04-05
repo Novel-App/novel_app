537e33e34ba289ab433b0d9452649b6abe3db1ab
akmr328 <akm328@gmail.com>
2021-04-05 10:45:18 -0400

    adds mobile styling for home page

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -8,6 +8,20 @@ export const Welcome = () => {
   return (
     <div className="welcome">
       <div>
+        <a href="/signup">
+          <btn
+            type="button"
+            id="signup"
+            className="btn btn-outline btn-primary"
+          >
+            Sign up
+          </btn>
+        </a>
+        <a href="/login">
+          <btn type="button" id="login" className="btn btn-outline btn-primary">
+            Login
+          </btn>
+        </a>
         <img id="novelgraphic" src="resources/images/novel_graphic.png" />
       </div>
     </div>
