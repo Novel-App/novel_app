8faf7d8d509a5a7a37d70dbba2bb5563bf6c4b58
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-04-05 09:36:47 -0400

    footer styling

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import * as AiIcons from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div>
-        <a href="https://coreui.io">CoreUI</a>
-        <span>&copy; 2020 creativeLabs.</span>
+    <section className="footer">
+      <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5 socialMedia">
+        <div className="mediaIconsFoot">
+          <ul className="list-unstyled list-inline social pull-right">
+            <li className="list-inline-item ">
+              <a href="#">
+                <AiIcons.AiFillFacebook className="footerIcon" />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">
+                <AiIcons.AiFillInstagram className="footerIcon" />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">
+                <AiIcons.AiFillTwitterCircle className="footerIcon" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="ml-auto">
-        <span>Powered by</span>
-        <a href="https://coreui.io">CoreUI</a>
-      </div>
-    </footer>
+    </section>
   )
 }
 
