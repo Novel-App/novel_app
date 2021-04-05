c3ff7c2f592454e341b110455c036a618d793801
akmr328 <akm328@gmail.com>
2021-04-05 17:25:51 -0400

    fixes manifest.json bug

--- client/components/Chats/SingleChat.js
+++ client/components/Chats/SingleChat.js
@@ -5,6 +5,7 @@ import {sendMessage, fetchMessages} from '../../store/message'
 import {fetchSingleChat} from '../../store/chat'
 import {Loading} from '../Loading'
 import {Link} from 'react-router-dom'
+import ScrollToBottom from 'react-scroll-to-bottom'
 
 /**
  * COMPONENT
@@ -24,22 +25,14 @@ export class SingleChat extends Component {
     this.submitChatMessage = this.submitChatMessage.bind(this)
   }
 
-  // scrollToBottom = () => {
-  //   this.messagesEnd.scrollIntoView({ behavior: "smooth" });
-  // }
+  chatContainer = React.createRef()
 
   componentDidMount() {
     const chatId = Number(this.props.match.params.chatId)
     this.props.getMessages(chatId)
     this.props.getChat(chatId)
     this.setState({loading: false})
-    // this.scrollToBottom()
   }
-
-  // componentDidUpdate() {
-  //   this.messagesEnd.scrollIntoView({behavior: 'smooth'})
-  //   this.scrollToBottom()
-  // }
 
   handleChange(e) {
     this.setState({
@@ -58,104 +51,102 @@ export class SingleChat extends Component {
     this.props.getChat(chatId)
     this.setState({content: '', messageSent: true})
   }
+
   render() {
     const chat = this.props.chat || {}
     // if (this.state.loading) {
     //   return <Loading />
     // }
     return (
-      <React.Fragment>
-        <div>
-          <div className="d-flex justify-content-between w-90">
-            <Link to="/chats/">
-              <div className="mt-1 ml-2">
-                <i
-                  className="bi bi-arrow-left-circle"
-                  style={{fontSize: '3em'}}
-                />
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div style={{maxWidth: '800px', margin: '0 auto'}}>
-          {chat.product && (
-            <div className="card mb-3">
-              <div className="row no-gutters">
-                <div className="col-md-4 d-flex justify-content-center align-items-center">
-                  <img
-                    src={chat.product.image[0]}
-                    className="card-img"
-                    alt="product-img"
-                    style={{height: '15vh', width: 'auto'}}
+      <ScrollToBottom>
+        <React.Fragment>
+          <div>
+            <div className="d-flex justify-content-between w-90">
+              <Link to="/chats/">
+                <div className="mt-1 ml-2">
+                  <i
+                    className="bi bi-arrow-left-circle"
+                    style={{fontSize: '3em'}}
                   />
                 </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <Link to={`/products/${chat.product.id}`}>
-                      <h5 className="card-title">
-                        {chat.product.title} by {chat.product.author}
-                      </h5>
-                    </Link>
-                    <p className="card-text text-muted">
-                      ${chat.product.price}
-                    </p>
-                    <p className="card-text">
-                      <small className="text-muted">
-                        {chat.product.availability}
-                      </small>
-                    </p>
+              </Link>
+            </div>
+          </div>
+          <div style={{maxWidth: '800px', margin: '0 auto'}}>
+            {chat.product && (
+              <div className="card mb-3">
+                <div className="row no-gutters">
+                  <div className="col-md-4 d-flex justify-content-center align-items-center">
+                    <img
+                      src={chat.product.image[0]}
+                      className="card-img"
+                      alt="product-img"
+                      style={{height: '15vh', width: 'auto'}}
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <Link to={`/products/${chat.product.id}`}>
+                        <h5 className="card-title">
+                          {chat.product.title} by {chat.product.author}
+                        </h5>
+                      </Link>
+                      <p className="card-text text-muted">
+                        ${chat.product.price}
+                      </p>
+                      <p className="card-text">
+                        <small className="text-muted">
+                          {chat.product.availability}
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+            <div className="px-md-3" id="messages">
+              <ul
+                id="messageFeed"
+                className="list-unstyled"
+                style={{height: '300px', overflowY: 'scroll'}}
+              >
+                <Message
+                  className="mr-3"
+                  chatId={this.props.match.params.chatId}
+                />
+                <div />
+              </ul>
+              <div className="input-group mb-3">
+                <div
+                  className="d-flex justify-content-end"
+                  onSubmit={this.submitChatMessage}
+                >
+                  <div className="align-items-center">
+                    <input
+                      id="messageTextarea"
+                      className="form-control"
+                      placeholder="Enter your message"
+                      type="text"
+                      value={this.state.content}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div className="input-group-append">
+                    <button
+                      type="submit"
+                      className="btn btn-outline-secondary d-flex justify-content-end"
+                      onClick={this.submitChatMessage}
+                    >
+                      {/* <Icon type="enter" /> */}
+                      send
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
-          )}
-          <div className="px-md-3" id="messages">
-            <ul
-              id="messageFeed"
-              className="list-unstyled"
-              style={{height: '200px', overflowY: 'scroll'}}
-            >
-              <Message
-                className="mr-3"
-                chatId={this.props.match.params.chatId}
-              />
-              <div
-              // ref={el => {
-              //   this.messagesEnd = el
-              // }}
-              // style={{float: 'left', clear: 'both'}}
-              />
-            </ul>
-            <div className="input-group mb-3">
-              <div
-                className="d-flex justify-content-end"
-                onSubmit={this.submitChatMessage}
-              >
-                <div className="align-items-center">
-                  <input
-                    id="messageTextarea"
-                    className="form-control"
-                    placeholder="Enter your message"
-                    type="text"
-                    value={this.state.content}
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <div className="input-group-append">
-                  <button
-                    type="submit"
-                    className="btn btn-outline-secondary d-flex justify-content-end"
-                    onClick={this.submitChatMessage}
-                  >
-                    {/* <Icon type="enter" /> */}
-                    send
-                  </button>
-                </div>
-              </div>
-            </div>
           </div>
-        </div>
-      </React.Fragment>
+        </React.Fragment>
+      </ScrollToBottom>
     )
   }
 }
