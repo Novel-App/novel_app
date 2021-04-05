34539a46834ccf02ee8afe5ff22d1b056f601df3
akmr328 <akm328@gmail.com>
2021-04-04 20:43:41 -0400

    home page styling updated

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
  */
 export const Welcome = () => {
   return (
-    <div>
+    <div className="welcome">
       {/* <div id="welcome-container" className="jumbotron jumbotron-fluid bg-dark">
         <div className="jumbotron-background">
           <img
@@ -26,6 +26,24 @@ export const Welcome = () => {
       </div> */}
       <div>
         <img id="novelgraphic" src="resources/images/novel_graphic.png" />
+        <a href="/signup">
+          <button
+            type="button"
+            id="welcome_round_left"
+            className="btn btn-primary btn-circle btn-xl text-wrap"
+          >
+            Sign up!
+          </button>
+        </a>
+        <a href="/login">
+          <button
+            type="button"
+            id="welcome_round_right"
+            className="btn btn-primary btn-circle btn-xl text-wrap"
+          >
+            Log in!
+          </button>
+        </a>
       </div>
     </div>
   )
