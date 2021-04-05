420fc85facf7b55e2c7455ba3257a10ec4f74f1d
akmr328 <akm328@gmail.com>
2021-04-04 20:44:46 -0400

    removes old code

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -7,23 +7,6 @@ import {Link} from 'react-router-dom'
 export const Welcome = () => {
   return (
     <div className="welcome">
-      {/* <div id="welcome-container" className="jumbotron jumbotron-fluid bg-dark">
-        <div className="jumbotron-background">
-          <img
-            src="https://cdn.lifestyleasia.com/wp-content/uploads/sites/2/2020/02/25145253/Photo-by-Alfons-Morales-on-Unsplash-scaled-1535x900.jpg"
-            className="blur "
-          />
-        </div>
-        <div className="container text-white">
-          <h1 className="display-4">Welcome to Novel</h1>
-          <h4 className="lead">Bringing books back to life</h4>
-          <Link to="/signup">
-            <button type="button" className="btn btn-outline-dark">
-              Sign Up Now
-            </button>
-          </Link>
-        </div>
-      </div> */}
       <div>
         <img id="novelgraphic" src="resources/images/novel_graphic.png" />
         <a href="/signup">
